cfdcb72dc01e84e42c2fbfd66db4ec8a737eccde
katros1 <katros250@gmail.com>
2024-07-17 22:28:18 +0200

    remove farm in signup

--- src/validation/user.validation.ts
+++ src/validation/user.validation.ts
@@ -17,7 +17,6 @@ const userSchema = Joi.object({
     username: Joi.string().min(3).trim().required(),
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'org', 'co'] } }).trim().required(),
     gender: Joi.string().required(),
-    farm: Joi.string().required(),
     phone:Joi.string().trim(),
     password: Joi.string().min(8).max(15).regex(/[0-9a-zA-Z]*\d[0-9a-zA-Z]*/).required(),
 });
